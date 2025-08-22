99a80f7097ed5919a266c1b4481369823d76fc4c
samuelrubiodev <samueldynamo11@gmail.com>
2025-08-22 13:35:18 +0200

    feat: update environment configuration and refactor extraction process to use webhook integration for n8n

--- src/app/api/extract/route.ts
+++ src/app/api/extract/route.ts
@@ -1,54 +1,31 @@
 "use server";
 
 import { NextResponse } from "next/server";
-import { extract } from '@extractus/article-extractor'
 import type { NextRequest } from "next/server";
-import { createOpenRouter } from '@openrouter/ai-sdk-provider';
-import { generateObject } from "ai";
-import { z } from "zod";
-
-import { PROMPT_SYSTEM } from "@/env/env";
-
-const openRouter = createOpenRouter({
-  apiKey: process.env.OPENROUTER_API_KEY,
-});
 
 export async function POST(req: NextRequest) {
   const { url } = await req.json();
 
   console.log("Starting extraction request");
-  try {
-    const article = await extract(url);
-    console.log("Extraction successful:", article);
 
-    const { object } = await generateObject({
-      model: openRouter("qwen/qwen3-235b-a22b-thinking-2507"),
-      prompt: `Please extract the following fields from the article:\n\n
-        Title: ${article?.title}\n\n Content: ${article?.content}\n\n`,
-      system: PROMPT_SYSTEM,
-      providerOptions: {
-        openrouter: {
-          provider: {
-            order: [
-              "parasail/fp8",
-              "deepinfra/fp8",
-              "cerebras",
-              "together"
-            ]
-          }
-        }
-      },
-      schema: z.object({
-        article: z.object({
-          title: z.string(),
-          author: z.string().nullable(),
-          date: z.string().nullable(),
-          source: z.string().nullable(),
-          summary: z.string(),
+  try {
+    const response = await fetch(
+      process.env.WEBHOOK_N8N_URL || "",
+      {
+        headers: {
+          Authorization: "Bearer " + process.env.WEBHOOK_TOKEN,
+          "Content-Type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify({
+          "url": url,
+          "language": "es"
         })
-      }),
-    });
-    return NextResponse.json(object, { status: 200 });
+      }
+    );
+    const result = await response.json();
+
+    return NextResponse.json(result, { status: 200 });
 
   } catch (error) {
     console.error("API extract error:", error); 
